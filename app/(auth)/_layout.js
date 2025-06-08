27a08647a712b3c21cf1766ade9fab2a11e7881d
Kenneth Kebaara <kkebaara@yahoo.com>
2025-06-08 17:58:01 -0600

    updated auth layout

--- app/(auth)/_layout.js
+++ app/(auth)/_layout.js
@@ -2,8 +2,9 @@ import { Stack } from 'expo-router';
 
 export default function AuthLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="login" options={{ title: 'Login' }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="login" />
+      <Stack.Screen name="register" />
     </Stack>
   );
 }
